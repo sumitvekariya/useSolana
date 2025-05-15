e5aafe206d065f774c7937dc5bdcb4b365c1277b
Sumit Vekariya <sarkazein7@gmail.com>
2025-05-15 16:21:54 +0530

    updated donation receiving address

--- src/utils/jobs.ts
+++ src/utils/jobs.ts
@@ -1,4 +1,4 @@
 export const NEWSLETTER_PRICE = 200
 export const ACCEPTED_NETWORK_IDS = [1, 10, 42161]
-export const RECEIVER_ADDRESS = '0xA512C3cF9A63715CEd87DE5058c684cBFF9f4321'
+export const RECEIVER_ADDRESS = '87VLPUTqgrKR3n9dZ72oytMXhUWM32rkpdTF9Vmfpjh5'
 export const RECEIVER_ENS = 'useweb3.eth'
